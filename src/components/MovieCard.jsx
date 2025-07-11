38950aec3e8cd81c889128ad28db6a15b255390d
Sebastian906 <elsebas1912@gmail.com>
2025-07-10 22:41:11 -0500

    Detalles adicionales en la información de las Cards

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -14,6 +14,12 @@ const MovieCard = ({ movie: { title, vote_average, poster_path, release_date, or
                         <img src='star.svg' alt='Star Icon'/>
                         <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
                     </div>
+                    <span>•</span>
+                    <p className='lang'>{original_language}</p>
+                    <span>•</span>
+                    <p className='year'>
+                        {release_date ? release_date.split('-')[0] : 'N/A'}
+                    </p>
                 </div>
             </div>
         </div>
